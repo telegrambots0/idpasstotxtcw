553a769ddc9bee57727f7ea37487f083bd180370
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-03-28 11:35:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ api_id = 20491966
 
 api_hash = "aa1c8f86db7f78fe9bfdd77bb48a5b23"
 
-bot_token "7183698213:AAFkrfBIiWN5lGcFH_OdK0bsggv2gcFOtCY"
+bot_token = "7183698213:AAFkrfBIiWN5lGcFH_OdK0bsggv2gcFOtCY"
 
